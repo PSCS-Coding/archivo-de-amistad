e550e439dc1385b928f7648d45413a9196db21f0
Anthony <anthony.reyes@pscs.org>
2017-12-22 23:50:55 -0800

    Added bootstrap

--- login.php
+++ login.php
@@ -1,9 +1,18 @@
 <!DOCTYPE html>
     <html>
-        <head>
-            <title>LOL</title>
-            <link rel="stylesheet" type="text/css" href="global.css">
-        </head>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>LOL REPO</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="bootstrap/dist/css/bootstrap.css" rel="stylesheet">
+
+</head>
         
         <body>
             
