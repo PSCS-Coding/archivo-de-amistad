f70691245193e7ac108fd6f7ff64babe7fca3cfd
Milo Rupp <badtzmaru139@gmail.com>
2017-12-23 00:13:34 -0800

    added error output

--- upload.php
+++ upload.php
@@ -1,4 +1,7 @@
 <?php
+
+require_once("connection.php");
+
 $target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
@@ -33,12 +36,15 @@ if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
     echo "Sorry, your file was not uploaded.";
+    header('Location: index.php?e=1');
 // if everything is ok, try to upload file
 } else {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
         echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+        header('Location: index.php?e=0');
     } else {
         echo "Sorry, there was an error uploading your file.";
+        header('Location: index.php?e=1');
     }
 }
 ?>
