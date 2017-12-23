8f85fb4f6b4edb60c9ea2a8bd905111283a4d626
Anthony <anthony.reyes@pscs.org>
2017-12-22 23:45:22 -0800

    Added base files.

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+    <html>
+        <head>
+            <title>Verge Beta</title>
+            <link rel="stylesheet" type="text/css" href="global.css">
+        </head>
+        
+        <body>
+            
+            <h1><b>[DEVELOPER BETA]</b></h1>
+            <table>
+                <th>Name</th>
+                <th>Status</th>
+                <tr>
+                    <td>
+                        Anthony
+                    </td>
+                    <td>
+                        Not Checked In.
+                    </td>
+                </tr>
+            </table>
+            
+        </body>
+    </html>
