0c28aa32cba8bb5f7a655109fe0bf2f98c85c172
Milo Rupp <badtzmaru139@gmail.com>
2017-12-24 14:34:56 -0800

    added ip checking

--- confirm.php
+++ confirm.php
@@ -1,6 +1,7 @@
 <?php
 require_once("connection.php");
 
+$ip = $_SERVER['REMOTE_ADDR']?:($_SERVER['HTTP_X_FORWARDED_FOR']?:$_SERVER['HTTP_CLIENT_IP']);
 $loginTest = "";
 $redirect = false;
 
@@ -16,6 +17,18 @@ if(!empty($_POST['password'])){
 
 $loginResult = $db_server->query("SELECT * FROM login");
 $loginRow = $loginResult->fetch_assoc();
+
+$ipResult = $db_server->query("SELECT * FROM ip WHERE adress = '".$ip."' ");
+if ($ipResult->num_rows != 1){
+    echo "You are not whitelisted to user this server!";
+    echo "<br>";
+    echo "This incident will be recorded!";
+    $stmt = $db_server->prepare("INSERT INTO logs (adress) VALUES (?)"); 
+    $stmt->bind_param('s', $ip); 
+    $stmt->execute(); 
+    $stmt->close();
+    header('Location: login.html?e=1');
+}
 
 if($loginTest == $loginRow['password']){
     setcookie("login",$loginRow['password']);
