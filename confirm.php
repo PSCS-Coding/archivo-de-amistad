a5facd17adad0610ca459c3795018ff6f295c5e0
Milo Rupp <badtzmaru139@gmail.com>
2017-12-23 20:03:32 -0800

    simple login

--- confirm.php
+++ confirm.php
@@ -5,10 +5,11 @@ $loginResult = $db_server->query("SELECT * FROM login");
 $loginRow = $loginResult->fetch_assoc();
 
 if(crypt($_POST['password'], 'P9') == $loginRow['password']){
+    setcookie("login",$loginRow['password']);
     header('Location: index.html?e=0'); 
 } else {
     header('Location: login.html?e=1'); 
 }
 
-// $crypt = crypt('adenz8r3ry8nyinynzyi', 'P9');
+
 ?>
